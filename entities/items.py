6ca5b71b7676c4246ed003406998319e0b08658b
David Rose <vido.ardes@gmail.com>
2016-10-28 23:47:18 +0100

    Fixed item attributes

--- entities/items.py
+++ entities/items.py
@@ -15,20 +15,20 @@ class CrustyBread(Consumable):
 
 class Potion(Consumable):
     def __init__(self):
-        self.name "Potion"
+        self.name = "Potion"
         self.healing_value = 25
         self.value = 20
 
 
 class HiPotion(Consumable):
     def __init__(self):
-        self.name "Hi-Potion"
+        self.name = "Hi-Potion"
         self.healing_value = 50
         self.value = 30
 
 
 class XPotion(Consumable):
     def __init__(self):
-        self.name "X-Potion"
+        self.name = "X-Potion"
         self.healing_value = 75
         self.value = 50
